b2b3361c16a16f4ccfddf4d00a06f7a739ad714b
nemethadel23 <153020477+nemethadel23@users.noreply.github.com>
2024-04-10 15:09:05 +0200

    Add files via upload

---
+++ fel07.sql
@@ -0,0 +1,15 @@
+/*1. Készítsünk nézetet VSZOBA néven, amely megjeleníti a szobák adatai mellett a megfelelő szálláshely nevét, helyét és a csillagok számát is!
+
+Az oszlopoknak nem szükséges külön nevet adni!
+Teszteljük is a nézetet, pl: SELECT * FROM UJAENB_VSZOBA*/
+
+
+/*2 Készítsen tárolt eljárást SPUgyfelFoglalasok, amely a paraméterként megkapott ügyfél azonosítóhoz tartozó foglalások adatait listázza!
+Teszteljük a tárolt eljárás működését, pl: EXEC UJAENB_SPUgyfelFoglalasok 'laszlo2'
+*/
+
+/*
+3. Készítsen skalár értékű függvényt UDFFerohely néven, amely visszaadja, hogy a paraméterként megkapott foglalás azonosítóhoz hány férőhelyes szoba tartozik!
+a. Teszteljük a függvény működését!
+*/
+
